2bdc0ff99d91c411681ff1037393baf726cd315b
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-04 23:27:42 -0700

    install devise and uncomment confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
     has_many :categories
     has_many :exchanges, foreign_key: 'author_id'
   
