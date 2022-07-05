09076a55f4ccac38fb98a6ca80cd3a9e2972275b
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-05 10:51:55 -0700

    Add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :validatable, :confirmable
+
+ ROLES = %i[admin default].freeze
+
+  def is?(requested_role)
+    role == requested_role.to_s
+    end
     has_many :categories
     has_many :exchanges, foreign_key: 'author_id'
   
