d498c86a30fc4b2d3ad34b2c68824400c9cb91bb
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-06 19:59:11 -0700

    Add splash view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index"
+  root 'splash#index'
+ resources :splash, only: [:index]
  resources :categories, only: [:index, :new, :create, :destroy]
+ 
 end
